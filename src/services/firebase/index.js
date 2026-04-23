9bdbe5855db886a843f43f79036152bf7c8db790
“Martina <martinaledesma2@gmail.com>
2026-04-23 17:34:38 -0300

    feat: conexion a firebase y estructura de base de datos

---
+++ src/services/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
+};
+
+// Inicializamos la aplicación
+const app = initializeApp(firebaseConfig);
+
+// Exportamos 'db' para poder usar Firestore en toda la app
+export const db = getFirestore(app);
